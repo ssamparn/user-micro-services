2e0bca9e41091c9fe7961d6d98a6634091dfca72
samantrs <https://samantrs@git.rabobank.nl>
2020-12-27 01:06:45 +0530

    removed command-line runner

--- user-management-service/src/main/java/com/microservices/userservice/UsersManagementServiceApplication.java
+++ user-management-service/src/main/java/com/microservices/userservice/UsersManagementServiceApplication.java
@@ -1,29 +1,14 @@
 package com.microservices.userservice;
 
-import com.microservices.userservice.properties.UserProperties;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class UsersManagementServiceApplication implements CommandLineRunner {
+public class UsersManagementServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UsersManagementServiceApplication.class, args);
-	}
-
-	@Autowired
-	private UserProperties properties;
-
-	@Override
-	public void run(String... args) {
-
-		System.out.println("@@@@@@@@@" + properties.getSecret());
-
-		System.out.println("@@@@@@@@" + properties.getExpirationTime());
-
 	}
 }
