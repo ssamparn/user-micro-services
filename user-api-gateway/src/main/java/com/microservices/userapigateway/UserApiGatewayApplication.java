34d7b6c767cbeaac5815a925ccaed12460bbf31b
samantrs <https://samantrs@git.rabobank.nl>
2020-12-26 16:36:07 +0530

    Lombok add to API Gateway

--- user-api-gateway/src/main/java/com/microservices/userapigateway/UserApiGatewayApplication.java
+++ user-api-gateway/src/main/java/com/microservices/userapigateway/UserApiGatewayApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class UserApiGatewayApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(UserApiGatewayApplication.class, args);
 	}
-
 }
